0b4ebdfec05b06fa7a97f606b6b34a23f1fff658
svitlana-p <sweetlana.pukish@gmail.com>
2022-11-29 11:27:56 +0000

    deleted about component

--- emotions-project/src/app/app-routing.module.ts
+++ emotions-project/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) }, 
   { path: 'test', loadChildren: () => import('./features/test/test.module').then(m => m.TestModule) }, 
   { path: 'result', loadChildren: () => import('./features/result/result.module').then(m => m.ResultModule) },
-  { path: 'about', loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule) }]
+  ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- emotions-project/src/app/features/about/about.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrls: ['./about.component.css']
-})
-export class AboutComponent { }
